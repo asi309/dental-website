e6d1799a8eb7576da4e6c7065785b25753859000
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2022-02-21 13:49:53 +0530

    updates testimonial component

--- src/container/Homepage/Testimonials/Testimonials.jsx
+++ src/container/Homepage/Testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import { IoEllipseOutline, IoEllipse } from 'react-icons/io';
+import { useEffect, useState } from 'react';
+import { IoEllipseOutline, IoEllipse } from 'react-icons/io5';
 
 import client from '../../../assets/client.jpg';
 import './Testimonials.css';
@@ -44,46 +44,120 @@ const testimonialData = [
 
 const TestimonialCard = ({
   testimonial: { title, text, name, role, imgUrl },
-}) => (
-  <div className="testimonial_card">
-    <div className="testimonial_card-blurb">
-      <h2 className="testimonial_card-blurb_title">{title}</h2>
-      <p className="testimonial_card-blurb_content">{text}</p>
-    </div>
-    <div className="testimonial_card-client">
-      <div className="testimonial_card-client_image">
-        <img src={imgUrl} alt="Testified by" />
+  isActive,
+  dots,
+  changeIndex,
+  isNext,
+  isPrev,
+  next,
+  prev,
+}) => {
+  return (
+    <div
+      className={`testimonial_card`}
+      style={isActive ? { marginTop: '-40px' } : { opacity: 0.75, cursor: 'pointer' }}
+      onClick={isNext ? next : isPrev ? prev : () => {}}
+    >
+      <div className="testimonial_card-blurb">
+        <h2 className="testimonial_card-blurb_title">{title}</h2>
+        <p className="testimonial_card-blurb_content">{text}</p>
       </div>
-      <div className="testimonial_card-client_details">
-        <h3 className="testimonial_card-client_name">{name}</h3>
-        <p className="testimonial_card-client_role">{role}</p>
+      <div className="testimonial_card-client">
+        <div className="testimonial_card-client_image">
+          <img src={imgUrl} alt="Testified by" />
+        </div>
+        <div className="testimonial_card-client_details">
+          <h3 className="testimonial_card-client_name">{name}</h3>
+          <p className="testimonial_card-client_role">{role}</p>
+        </div>
       </div>
+      {isActive && (
+        <div className="testimonial_card-ellipsis">
+          <ul>
+            {dots.map((dot, idx) => (
+              <li key={idx} onClick={() => changeIndex(idx)}>
+                {dot}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
-const Testimonials = () => {
-  const scrollRef = useRef(null);
+const TestimonialSlider = ({ testimonialData }) => {
+  const totalItems = testimonialData.length;
+  const [index, setIndex] = useState(0);
+  const [prevItems, setPrevItems] = useState(totalItems - 1);
+  const [nextItems, setNextItems] = useState(1);
+  const [sliderItems, setSliderItems] = useState([prevItems, index, nextItems]);
 
-  const scroll = (direction) => {
-    const { current } = scrollRef;
+  const dots = [];
 
-    if (direction === 'left') {
-      current.scrollLeft -= 410;
+  for (let i = 0; i < totalItems; i++) {
+    if (i === index) {
+      dots[i] = <IoEllipseOutline fontSize={18} />;
     } else {
-      current.scrollLeft += 410;
+      dots[i] = <IoEllipse fontSize={14} />;
+    }
+  }
+
+  const changePrevNext = () => {
+    if (index === totalItems - 1) {
+      setNextItems(0);
+    } else {
+      setNextItems(index + 1);
+    }
+    if (index === 0) {
+      setPrevItems(totalItems - 1);
+    } else {
+      setPrevItems(index - 1);
     }
   };
 
-  // useEffect(() => {
-  //   setTimeout();
-  
-  //   return () => {
-  //     second;
-  //   };
-  // }, [third]);
-  
+  const next = () => {
+    setIndex((currIdx) => (currIdx + 1) % totalItems);
+  };
 
+  const prev = () => {
+    setIndex((currIdx) => (currIdx - 1 + totalItems) % totalItems);
+  };
+
+  const changeIndex = (idx) => {
+    if (idx !== index) {
+      setIndex(idx);
+    }
+  };
+
+  useEffect(() => {
+    changePrevNext();
+  }, [index]);
+
+  useEffect(() => {
+    setSliderItems([prevItems, index, nextItems]);
+  }, [prevItems, nextItems]);
+
+  return (
+    <div className="app__testimonials-content_scroll">
+      {sliderItems.map((testimonialIdx, idx) => (
+        <TestimonialCard
+          testimonial={testimonialData[testimonialIdx]}
+          key={'testimonial-' + idx}
+          isActive={index === testimonialIdx}
+          dots={dots}
+          changeIndex={changeIndex}
+          isNext={nextItems === testimonialIdx}
+          isPrev={prevItems === testimonialIdx}
+          next={next}
+          prev={prev}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Testimonials = () => {
   return (
     <div className="app__testimonials">
       <div className="app__testimonials-header">
@@ -93,14 +167,7 @@ const Testimonials = () => {
         </div>
       </div>
       <div className="app__testimonials-content">
-        <div className="app__testimonials-content_scroll" ref={scrollRef}>
-          {testimonialData.map((testimonial, idx) => (
-            <TestimonialCard
-              key={testimonial + idx}
-              testimonial={testimonial}
-            />
-          ))}
-        </div>
+        <TestimonialSlider testimonialData={testimonialData} />
       </div>
     </div>
   );
